5b992e27aed146c85fb4d3b19f2115e6129b8f0a
jxhnkndl <jkroyston@gmail.com>
2025-03-22 14:57:34 -0400

    add background video and logo to hero section

--- templates/home.php
+++ templates/home.php
@@ -3,7 +3,17 @@
 <?php get_header(); ?>
 
 <main>
-  <h1>SUPERTREE</h1>
+  <section class="hero-section">
+    <video class="hero-section-video-container" autoplay muted loop>
+      <source src="<?php echo get_template_directory_uri(  ) . '/assets/video/supertree-hero-video.mp4' ?>">
+    </video>
+    <div class="hero-section-content-container">
+      <div class="hero-section-logo-container">
+        <img src="<?php echo get_template_directory_uri() . '/assets/images/supertree_logo.png' ?>" alt="Supertree is a powerhouse 90s alternative rock cover band" class="hero-section-logo-img">
+      </div>
+      <button class="btn cta-btn-mobile">Book Now</button>
+    </div>
+  </section>
 </main>
 
 <?php get_footer(); ?>
