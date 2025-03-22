a192d4c1fe342c25f774ac1e24dea327dbe88c85
jxhnkndl <jkroyston@gmail.com>
2025-03-22 15:43:54 -0400

    adjust cta button styles on mobile

--- templates/home.php
+++ templates/home.php
@@ -7,7 +7,7 @@
     <video class="hero-section-video-container" autoplay muted loop>
       <source src="<?php echo get_template_directory_uri(  ) . '/assets/video/supertree-hero-video.mp4' ?>">
     </video>
-    <div class="hero-section-content-container">
+    <div class="hero-section-content-container side-padding">
       <div class="hero-section-logo-container">
         <img src="<?php echo get_template_directory_uri() . '/assets/images/supertree_logo.png' ?>" alt="Supertree is a powerhouse 90s alternative rock cover band" class="hero-section-logo-img">
       </div>
