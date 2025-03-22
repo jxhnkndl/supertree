c2da6fd5dba2dc5faab5ac9e7f5d1cbc97e3961b
jxhnkndl <jkroyston@gmail.com>
2025-03-22 16:05:25 -0400

    convert cta button from button tags into a tags

--- templates/home.php
+++ templates/home.php
@@ -3,6 +3,7 @@
 <?php get_header(); ?>
 
 <main>
+  <!-- Hero Section -->
   <section class="hero-section">
     <video class="hero-section-video-container" autoplay muted loop>
       <source src="<?php echo get_template_directory_uri(  ) . '/assets/video/supertree-hero-video.mp4' ?>">
@@ -11,7 +12,7 @@
       <div class="hero-section-logo-container">
         <img src="<?php echo get_template_directory_uri() . '/assets/images/supertree_logo.png' ?>" alt="Supertree is a powerhouse 90s alternative rock cover band" class="hero-section-logo-img">
       </div>
-      <button class="btn cta-btn-mobile">Book Now</button>
+      <a class="btn cta-btn-mobile">Book Now</a>
     </div>
   </section>
 </main>
