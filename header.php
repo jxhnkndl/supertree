d47da2468df6345a79e60f9c0d2a8a4b52ef0afe
jxhnkndl <jkroyston@gmail.com>
2025-03-23 09:49:27 -0400

    build out desktop nav links and cta button

--- header.php
+++ header.php
@@ -6,11 +6,36 @@
   <?php wp_head(); ?>
 </head>
 <body>
+  <!-- Main Header -->
   <header class="header">
-    <div class="container header-container">
-      <div class="header-logo">
-        <img src="<?php echo get_template_directory_uri() . '/assets/images/triple-infinity-orange.svg' ?>" alt="Supertree is a powerhouse 90s alternative rock cover band" class="logo-icon"/>
-        Supertree
+    <div class="header-container">
+      <div class="header-main-nav">
+        <!-- Brand Link -->
+        <a href="#" class="header-logo-link">
+          <div class="header-logo">
+            <img src="<?php echo get_template_directory_uri() . '/assets/images/triple-infinity-orange.svg' ?>" alt="Supertree is a powerhouse 90s alternative rock cover band" class="logo-icon"/>
+            Supertree
+          </div>
+        </a>
+        <!-- Desktop Nav Links -->
+        <nav class="nav-desktop">
+          <ul class="nav-desktop-links">
+            <li class="nav-desktop-link-container">
+              <a href="#media" class="nav-desktop-link">Media</a>
+            </li>
+            <li class="nav-desktop-link-container">
+              <a href="#about" class="nav-desktop-link">About</a>
+            </li>
+            <li class="nav-desktop-link-container">
+              <a href="#concerts" class="nav-desktop-link">Concerts</a>
+            </li>
+            <li class="nav-desktop-link-container">
+              <a href="#contact" class="nav-desktop-link">Contact</a>
+            </li>
+          </ul>
+        </nav>
       </div>
+      <!-- Desktop Header CTA Button -->
+      <a href="#contact" class="header-cta-btn">Book Now</a>
     </div>
   </header>
