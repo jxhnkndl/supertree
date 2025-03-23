5a6637674582703a4e33ce81462ddf7dc4ab841b
jxhnkndl <jkroyston@gmail.com>
2025-03-23 08:25:23 -0400

    add artist name and icon to header

--- header.php
+++ header.php
@@ -6,6 +6,11 @@
   <?php wp_head(); ?>
 </head>
 <body>
-  <header>
-    I'm a header
+  <header class="header">
+    <div class="container header-container">
+      <div class="header-logo">
+        <img src="<?php echo get_template_directory_uri() . '/assets/images/triple-infinity-orange.svg' ?>" alt="Supertree is a powerhouse 90s alternative rock cover band" class="logo-icon"/>
+        Supertree
+      </div>
+    </div>
   </header>
