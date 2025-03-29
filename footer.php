b708f757cf724362b8c5654d2a0011eb67599563
jxhnkndl <jkroyston@gmail.com>
2025-03-29 09:01:00 -0400

    add footer content

--- footer.php
+++ footer.php
@@ -1,5 +1,20 @@
-  <footer>
-    I'm a footer
+<footer>
+    <div class="side-padding">
+      <div class="container">
+        <div class="footer-content">
+          <p class="footer-text">Copyright © 2025 Supertree</p>
+          <p class="footer-text">All Rights Reserved</p>
+          <div class="social-icons-container">
+            <a class="social-icon-link" href="https://www.youtube.com/@supertreemusic">
+              <img src="<?php echo get_template_directory_uri() . '/assets/images/youtube_icon.svg' ?>" alt="Visit Supertree's YouTube profile">
+            </a>
+            <a class="social-icon-link" href="https://www.instagram.com/supertree_music/">
+              <img src="<?php echo get_template_directory_uri() . '/assets/images/instagram_icon.svg' ?>" alt="Visit Supertree's Instagram profile">
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
   </footer>
   <?php wp_footer() ?>
 </body>
