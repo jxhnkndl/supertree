239d178410eab9d27456d7de627f188928e4430f
jxhnkndl <jkroyston@gmail.com>
2025-07-27 08:37:41 -0400

    third attempt at refactoring media gallery

--- assets/js/media-player.js
+++ assets/js/media-player.js
@@ -1,39 +1,26 @@
-
-const videoPlayer = document.querySelector('iframe');
+const videoPlayer1 = document.querySelector('.video-player-1');
+const videoPlayer2 = document.querySelector('.video-player-2');
+const videoPlayer3 = document.querySelector('.video-player-3');
 const videoPlayBtn1 = document.querySelector('.play-icon-1');
 const videoPlayBtn2 = document.querySelector('.play-icon-2');
 const videoPlayBtn3 = document.querySelector('.play-icon-3');
 
-const videos = [
-    {
-        artist: "The Smashing Pumpkins",
-        title: "Cherub Rock",
-        src: 'https://www.youtube.com/embed/9vuMayqtlSc?si=Kh5nmuedXC_Zy3sM'
-    },
-    {
-        artist: "Plush",
-        title: "Stone Temple Pilots",
-        src: 'https://www.youtube.com/embed/jeyIPKoDyqk?si=Fgu8sZO9fa49E5qW'
-    },
-    {
-        artist: "Even Flow",
-        title: "Pearl Jam",
-        src: 'https://www.youtube.com/embed/02r-juLqLfI?si=gm1sWtQqwv_b0Q1b'
-    },
-];
-
-const toggleVideo = (index) => {
-    videoPlayer.src = videos[index].src;
-}
+// Toggle videos in media player
 
 videoPlayBtn1.addEventListener('click', () => {
-    toggleVideo(0)
+    videoPlayer1.classList.replace('hide-video', 'show-video');
+    videoPlayer2.classList.replace('show-video', 'hide-video');
+    videoPlayer3.classList.replace('show-video', 'hide-video');
 });
 
 videoPlayBtn2.addEventListener('click', () => {
-    toggleVideo(1)
+    videoPlayer1.classList.replace('show-video', 'hide-video');
+    videoPlayer2.classList.replace('hide-video', 'show-video');
+    videoPlayer3.classList.replace('show-video', 'hide-video');
 });
 
 videoPlayBtn3.addEventListener('click', () => {
-    toggleVideo(2)
+    videoPlayer1.classList.replace('show-video', 'hide-video');
+    videoPlayer2.classList.replace('show-video', 'hide-video');
+    videoPlayer3.classList.replace('hide-video', 'show-video');
 });
